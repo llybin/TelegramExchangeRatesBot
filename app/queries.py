a19c00ea775ed409fcdd072b5213ca94fe560e34
Lev Lybin <lev.lybin@gmail.com>
2019-04-04 00:44:01 +0700

    order currency in menu

--- app/queries.py
+++ app/queries.py
@@ -6,4 +6,4 @@ from app.models import Currency
 
 @region.cache_on_arguments(expiration_time=300)
 def get_all_currencies():
-    return [x[0] for x in Session.query(Currency.code).filter_by(is_active=True)]
+    return [x[0] for x in Session.query(Currency.code).filter_by(is_active=True).order_by(Currency.code)]
