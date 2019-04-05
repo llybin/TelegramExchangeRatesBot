f7537f116f7a49663c40ec06a3731f68075c6ed0
Lev Lybin <lev.lybin@gmail.com>
2019-04-05 14:27:54 +0700

    fix to avoid moving buttons when they have similar counter

--- app/queries.py
+++ app/queries.py
@@ -27,7 +27,7 @@ def get_last_request(chat_id):
     return Session.query(ChatRequests).filter_by(
         chat_id=chat_id
     ).order_by(
-        ChatRequests.times.desc()
+        ChatRequests.times.desc(), ChatRequests.id
     ).limit(size).all()
 
 
