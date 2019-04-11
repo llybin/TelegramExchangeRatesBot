e01e717a718be138ef72d1f5d689f9e1f0f3caa2
Lev Lybin <lev.lybin@gmail.com>
2019-04-12 00:34:31 +0700

    drop donation section

--- app/callbacks/help.py
+++ app/callbacks/help.py
@@ -34,29 +34,6 @@ def help_callback(update: Update, context: CallbackContext, chat_info: dict, _: 
     text_to += _("Don't have your localization? Any translation errors? Help fix it 👉 [poeditor.com](%(trans_link)s)") % {  # NOQA
         'trans_link': 'https://poeditor.com/join/project/LLu8AztSPb'}
 
-    text_to += '\n\n'
-
-    text_to += '*Donations*'
-
-    text_to += '\n\n'
-
-    text_to += '''[PayPal](https://paypal.me/lybin)
-
-[Yandex.Money](money.yandex.ru/to/41001252061112)
-
-*WebMoney*: R152423429164, Z282158953669, X159713546826, E378035973063
-
-*Bitcoin (BTC)*: 1GJZ36ahmApeX6RuuDGhi7t8qzBprJ9sQc
-
-*Bitcoin Cash (BCH)*: qznahv6kprwsfrq6sr2ehcfuf259yjcmqgk7fuz976
-
-*Ether (ETH)*: 0x56b2144aFE4564852409B302B29d7B6B2797Cf2D
-
-*BurstCoin (BURST)*: BURST-BTKF-8WT9-L98N-98JH2
-
-*Stellar (XLM)*: GBP332VIMDGSPJOOCXPCAJSS3EDBTYLJ4IVY42674F436ZXCVKVJWJY4
-'''
-
     update.message.reply_text(
         disable_web_page_preview=True,
         parse_mode=ParseMode.MARKDOWN,
