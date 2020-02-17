5c2e8becc0fd54afd6f4c9b427859f2f36b62f8f
Lev Lybin <lev.lybin@gmail.com>
2020-02-17 13:29:35 +0700

    satang exchanger

--- app/settings.py
+++ app/settings.py
@@ -59,6 +59,12 @@ CELERYBEAT_SCHEDULE = {
         "args": ("app.exchanges.BitkubExchange",),
         "options": {"time_limit": 60, "once": {"timeout": 60}},
     },
+    "exchange_updater_SatangExchange": {
+        "task": "app.tasks.exchange_updater",
+        "schedule": crontab(minute="*/1"),
+        "args": ("app.exchanges.SatangExchange",),
+        "options": {"time_limit": 60, "once": {"timeout": 60}},
+    },
     "exchange_updater_OpenExchangeRatesExchange": {
         "task": "app.tasks.exchange_updater",
         "schedule": crontab(minute=1, hour="*/1"),
