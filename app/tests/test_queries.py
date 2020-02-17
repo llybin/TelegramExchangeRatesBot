64bfeaf3c489d2c0c939388b748610fc6264b799
Lev Lybin <lev.lybin@gmail.com>
2020-02-17 13:35:49 +0700

    fix test after adding one more currency

--- app/tests/test_queries.py
+++ app/tests/test_queries.py
@@ -4,4 +4,4 @@ from suite.test.testcases import SimpleTestCase
 
 class GetAllCurrenciesTest(SimpleTestCase):
     def test_get_all_currencies(self):
-        self.assertEqual(len(get_all_currency_codes()), 210)
+        self.assertEqual(len(get_all_currency_codes()), 211)
